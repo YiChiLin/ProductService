1f407aba3a072a915c3a90d1a49064193399db5e
YiChi Lin <sky61054@gmail.com>
2017-06-13 16:55:07 +0800

    revert

---
+++ ProductService/ProductService/Service.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Service
+{
+    internal class Service<T>
+    {
+      
+        public Service()
+        {
+        }
+
+        internal List<T> GetData(List<T> data)
+        {
+            return new List<T>();
+        }
+
+        internal List<T> GetData()
+        {
+            return new List<T>();
+        }
+
+        internal List<int> GetSum(int groupRows,string column)
+        {
+            return new List<int>();
+        }
+    }
+}
